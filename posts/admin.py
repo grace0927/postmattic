64507098716366b69be6ca17546e209d1a14effd
Jianyu Feng <cellphone2008@gmail.com>
2018-11-03 17:49:57 -0400

    add nav item

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from posts.models import NavItem
 
-# Register your models here.
+# Register your models here, so that it can be modified in admin page.
+admin.site.register(NavItem)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from posts.models import NavItem
+from rest_framework import viewsets
+from posts.serializers import NavItemSerializer
 
-# Create your views here.
+
+class NavItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows nav item to be viewed or edited.
+    """
+    queryset = NavItem.objects.all()
+    serializer_class = NavItemSerializer
